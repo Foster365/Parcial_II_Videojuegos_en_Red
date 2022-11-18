f58d53b16e39e6cc9df381df3a08a0ddbf5a4e37
OctavioCapozzoli <octavio.capozzoli@hotmail.com>
2022-11-18 02:01:36 -0300

    map + pickup script
    a

---
+++ Assets/Scripts/PickUpController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    public Launcher launcherScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
+    {
+        if (!equipped)
+        {
+            launcherScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && !slotFull && Input.GetKeyDown(KeyCode.Q)) PickUp();
+    }
+
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        launcherScript.enabled = true;
+    }
+}
